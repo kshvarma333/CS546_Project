2653041baaba469071cd0e72f45ef9b8600760d6
Chirag7197 <nitil@Nitils-MacBook-Pro.local>
2019-11-24 01:26:17 -0500

    Added Routes

--- src/data/events.js
+++ src/data/events.js
@@ -1,6 +1,7 @@
 const mongoCollections = require('./mongoCollections');
 const events = mongoCollections.events;
-//const {ObjectId}=require('mongodb');
+const {ObjectId}=require('mongodb');
+
 
 const exportedMethods = {
   async getAllEvents() {
@@ -10,17 +11,49 @@ const exportedMethods = {
   },
 
   async getEvent(id) {
+    if(typeof(id) !== "string" )
+    id=id.toString();
+
     const eventsCollection = await events();
-    const event = await eventsCollection.findOne({_id: ObjectId(id)});
-    return event;
+    const gevent = await eventsCollection.findOne({_id: ObjectId(id)});
+
+    if(!gevent)
+    throw "Event Of ID "+id+" Not Found"
+
+    return gevent;
   },
-  
+
+  async createEvent(eventInfo) {
+
+    const eventCollection = await events();
+    let newEvent={
+      name: eventInfo.eventName,
+      description: eventInfo.eventDesc,
+      createdBy: eventInfo.createdBy,
+      location: eventInfo.location,
+      tourGuide: eventInfo.tourGuide,
+      price: eventInfo.price,
+      maxUsers: eventInfo.maxUsers,
+      regdUsersCount: eventInfo.regdUsersCount,
+      regdUsers: {},
+      eventDate: eventInfo.eventDate,
+      eventStatus: eventInfo.eventStatus
+    }
+    const insertEvent = await eventCollection.insertOne(newEvent);
+    if(insertEvent.insertedCount == 0)
+    throw "Could not add Event";
+
+    const Id = insertEvent.insertedId;
+    const eve = exportedMethods.getEvent(Id);
+    return eve;
+  },
+
   async updateEvent(id,update) {
     id=id.toString()
     if (!id || !update)
     {
       throw "No update";
-    } 
+    }
     const eventsCollection = await events();
     const updatedEvent = { $set:update
     };
@@ -32,14 +65,16 @@ const exportedMethods = {
     return await this.getEvent(id);
   },
 
-  
+
   async deleteEvent(id) {
     const eventsCollection = await events();
-    const deleted = await eventsCollection.removeOne({ _id: ObjectId(id) });    
-    return deleted;
+    const delEvent = await eventsCollection.findOne({ _id: ObjectId(id) });
+
+    const deleted = await eventsCollection.removeOne({ _id: ObjectId(id) });
+    return delEvent;
   },
-  
-  async getTopEvents(amount=5) {
+
+  async getTopEvents() {
     const eventsCollection = await events();
     const allevents = await eventsCollection.find({}).sort({rating : -1}).limit(5).toArray();
     return allevents;
@@ -47,10 +82,10 @@ const exportedMethods = {
   async setRateEvent(id,rate) {
     // Need to work on.
     const eventsCollection = await events();
-    const allevents = await eventCollection.find({_id: ObjectId(id}).toArray();
+    const allevents = await eventCollection.find({_id: ObjectId(id)}).toArray();
     return allevents;
   },
-  
+
 
 };
 
